be90f67300b4bd152308a16d99aff745fa3e2923
Martin Natale <martin01.natale@gmail.com>
2024-05-25 17:36:38 +0200

    feat: Add the list of model

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
+    list_oracle = ['groq_llama3-70b-8192', 'groq_mixtral-8x7b-32768']
+    list_student = ['groq_gemma-7b-it', 'groq_llama3-8b-8192']
     if request.method == 'POST':
         try:
             data = request.get_json(force=True)
             theme = data.get('theme', 'Coding Data Structures and Algorithms exerices in Python')
-            oracle = data.get('model', 'MistralLarge') #TODO Vérifier que c'est bien dans une liste
-            student_model = data.get('student_model', 'Phi')
+            oracle = data.get('model', 'groq_llama3-70b-8192')
+            student_model = data.get('student_model', 'groq_gemma-7b-it')
+            if oracle not in list_oracle:
+                return jsonify(error=f"Oracle model {oracle} not found"), 400
+            if student_model not in list_student:
+                return jsonify(error=f"Student model {student_model} not found"), 400
             return jsonify(message=f"Hello World {theme}!")
         except Exception as e:
             return jsonify(error=str(e)), 400
@@ -17,7 +23,7 @@ def main():
 
 @app.route('/get_response', methods=['GET'])
 def get_response():
-
+    return "Hello World!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)

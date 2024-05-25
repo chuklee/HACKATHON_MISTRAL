5f5f1cc2f4d70d0863d7dccb90850b243cfacbd8
eithan <eithan.nakache@epita.fr>
2024-05-25 23:14:35 +0200

    feat(Interface): add api keys

--- web/1_🏠_Home.py
+++ web/1_🏠_Home.py
@@ -2,7 +2,13 @@ import streamlit as st
 import requests
 from utils import load_models, create_new_page
 
-st.set_page_config(page_title="smol. 🦎", page_icon="🦎", layout="wide" , initial_sidebar_state="collapsed")
+st.set_page_config(page_title="smol. 🦎", page_icon="🦎", layout="wide", initial_sidebar_state="collapsed")
+
+# Initialize session state variables if they don't exist
+if "delete_model" not in st.session_state:
+    st.session_state["delete_model"] = False
+if "model_name_to_delete" not in st.session_state:
+    st.session_state["model_name_to_delete"] = ""
 
 # Title and description
 st.title("smol. 🦎")
@@ -10,7 +16,14 @@ st.sidebar.write("")
 
 # Main container
 with st.container():
+    st.header("API Configuration")
+    
+    GROQ_API_KEY= st.text_input("GROQ API Key")  
+    FIREWORKS_API_KEY = st.text_input("FIREWORKS API Key")
+    MISTRAL_API_KEY = st.text_input("MISTRAL API Key")
     oracles, students = load_models()
+    button_submit_key = st.button("Submit API Keys 🔑")
+    
     st.header("Configuration")
     name_input = st.text_input("Name")
     theme_input = st.text_input("Theme")
@@ -18,10 +31,19 @@ with st.container():
     student_model_input = st.selectbox("Student", students)
     button_train = st.button("Train Model 🚀")
 
+if button_submit_key:
+    if GROQ_API_KEY and FIREWORKS_API_KEY and MISTRAL_API_KEY:
+        api_endpoint = "http://"
+        payload = {
+            "GROQ_API_KEY": GROQ_API_KEY,
+            "FIREWORKS_API_KEY": FIREWORKS_API_KEY,
+            "MISTRAL_API_KEY": MISTRAL_API_KEY
+        }
+        response = requests.post(api_endpoint, json=payload)
+        st.success(f"API Keys submitted successfully!")
 
 if button_train:
     if theme_input and oracle_input and student_model_input:
-        
         api_endpoint = "http://127.0.0.1:105/create_model"
         payload = {
             "theme": theme_input,
@@ -30,7 +52,6 @@ if button_train:
         }
         response = requests.post(api_endpoint, json=payload)
         create_new_page(name_input, theme_input, oracle_input, student_model_input)
-        
+        st.success(f"Page {name_input} created successfully!")
     else:
         st.error("Please fill in all fields before starting the training.")
-
--- web/utils.py
+++ web/utils.py
@@ -5,7 +5,7 @@ MODEL_PATH = "../models.json"
 def read_json(path: str) -> dict:
     with open(path, 'r') as f:
         data = json.load(f)
-        return data
+    return data
     
 def load_models():
     models = read_json(MODEL_PATH)
@@ -13,13 +13,11 @@ def load_models():
     students = models["student"]
     return oracles, students
 
-
-
 def create_new_page(name_input, theme_input, oracle_input, student_model_input):
     content = f"""import streamlit as st
 import os
 import time
-        
+
 st.set_page_config(page_title="Model Details", page_icon="🤖")
 
 st.title("Model Details")
@@ -27,20 +25,29 @@ st.write(f"**Name**: {name_input}")
 st.write(f"**Theme**: {theme_input}")
 st.write(f"**Oracle**: {oracle_input}")
 st.write(f"**Student**: {student_model_input}")
-st.text_area("")
-submit = st.button('Generate')  
 
-if submit:
-    with st.spinner(text="This may take a moment..."):
-        time.sleep(2)
-        text2 = "response_message"
-        st.write(text2)
-
+messages = st.container(height=300)
+if prompt := st.chat_input("Say something"):
+    messages.chat_message("user").write(prompt)
+    messages.chat_message("assistant").write("On attend Camil pour répondre à votre question.")
 
 if st.button(f"Delete Model {name_input}"):
-    os.remove(f"pages/{name_input}.py")
-    st.session_state.runpage= "Home"
-    st.rerun()
+    st.session_state["delete_model"] = True
+    st.session_state["model_name_to_delete"] = "{name_input}"
+    st.experimental_rerun()
+
+if "delete_model" in st.session_state and st.session_state["delete_model"]:
+    if st.session_state["model_name_to_delete"] == "{name_input}":
+        model_file_path = f"pages/{name_input}.py"
+        if os.path.exists(model_file_path):
+            os.remove(model_file_path)
+            st.success(f"Model {name_input} deleted successfully!")
+        else:
+            st.error(f"Model {name_input} does not exist!")
+        st.session_state["delete_model"] = False
+        st.session_state["model_name_to_delete"] = ""
+        st.experimental_set_query_params(page="home")
+        st.experimental_rerun()
 """
     filename = f"pages/{name_input}.py"
     with open(filename, "w") as file:
